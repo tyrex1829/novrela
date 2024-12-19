a746f5875eeea8375157038f219e92a352ed35ac
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-19 23:58:31 +0530

    setup connection poooling with db logic

---
+++ backend/src/db/index.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import env from "dotenv";
+env.config();
+
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+}).$extends(withAccelerate());
