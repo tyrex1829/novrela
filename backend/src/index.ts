b7a4e6c6694e1ad024b7b67079535af2e5c8168d
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-20 02:40:17 +0530

    backend without blogs feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import { Hono } from "hono";
-import indexRouter from "./routes/indexV1.js";
+import indexRouter from "./routes/indexV1";
 import { cors } from "hono/cors";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
+  };
+  variable: {
+    userId: string;
   };
 }>();
 
