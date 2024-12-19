027927df77388afcc668a1fc0b00e3042a179ab0
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-20 01:06:50 +0530

    Feat: bindings and cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,15 @@
 import { Hono } from "hono";
 import indexRouter from "./routes/indexV1.js";
+import { cors } from "hono/cors";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+app.use(cors());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
