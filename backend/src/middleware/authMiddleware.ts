ded737d700867d9af0d228c26a1f2948f4610f1a
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-20 14:28:22 +0530

    Fix: await in next

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -2,23 +2,38 @@ import { Context, Next } from "hono";
 import { verify } from "hono/jwt";
 
 export const auth = async (c: Context, next: Next) => {
-  const jwt = c.req.header("Authorization");
+  try {
+    const jwt = c.req.header("authorization") || "";
 
-  if (!jwt) {
-    c.status(401);
-    return c.json({
-      error: `Unauthorised`,
-    });
+    if (!jwt) {
+      c.status(401);
+      return c.json({
+        error: `No token provided`,
+      });
+    }
+
+    const token = jwt.split(" ")[1];
+
+    if (!token) {
+      return c.json({ error: "Invalid token format" }, 401);
+    }
+
+    const payload = await verify(token, c.env.JWT_SECRET);
+
+    if (!payload) {
+      c.status(401);
+      return c.json({ error: "Invalid token" });
+    }
+
+    c.set("userId", payload.id);
+
+    await next();
+  } catch (error) {
+    return c.json(
+      {
+        message: `Auth failed: ${error}`,
+      },
+      500
+    );
   }
-
-  const token = jwt.split("")[1];
-
-  const payload = await verify(token, c.env.JWT_SECRET);
-
-  if (!payload) {
-    c.status(401);
-    return c.json({ error: "unauthorized" });
-  }
-  c.set("userId", payload.id);
-  await next();
 };
