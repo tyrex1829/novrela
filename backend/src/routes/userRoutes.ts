a4017a427f09f6678ad6613300dc6c51398f2870
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-20 11:06:12 +0530

    Fix: routing of signin route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,9 +2,17 @@ import { Hono } from "hono";
 import { signup } from "../controller/userController";
 import { signin } from "../controller/userController";
 
-const userRouter = new Hono();
+const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  variable: {
+    userId: string;
+  };
+}>();
 
 userRouter.post("/signup", signup);
-userRouter.post("/signup", signin);
+userRouter.post("/signin", signin);
 
 export default userRouter;
