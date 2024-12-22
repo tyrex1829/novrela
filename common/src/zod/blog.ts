d1c8c1f4dc1af3eeec50022a96778d53f0194e88
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-23 04:07:07 +0530

    Fix: schema validation of createing a blog post

--- common/src/zod/blog.ts
+++ common/src/zod/blog.ts
@@ -3,16 +3,16 @@ import z from "zod";
 export const makeBlogPost = z.object({
   title: z.string(),
   content: z.string().min(10),
-  published: z.boolean(),
-  authorId: z.string(),
+  published: z.boolean().optional(),
+  authorId: z.string().optional(),
 });
 
 export const updateBlogPost = z.object({
   id: z.string(),
   title: z.string(),
   content: z.string().min(10),
-  published: z.boolean(),
-  authorId: z.string(),
+  published: z.boolean().optional(),
+  authorId: z.string().optional(),
 });
 
 export type MakeBlog = z.infer<typeof makeBlogPost>;
