67638aaf2434a89838eddb047afc9b02a7878ddf
Saksham Khare <sakshamkhare19@gmail.com>
2024-12-23 04:22:43 +0530

    complete project with backend config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 // import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Blog } from "./pages/Blog";
+import { Blogs } from "./pages/Blogs";
+import { Publish } from "./pages/Publish";
+import { Landing } from "./pages/Landing";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -9,9 +15,12 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
     </>
